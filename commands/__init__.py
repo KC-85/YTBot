2a19336de5fa2ec1260cc1fcc24d1c7728ff0e59
Kristian Cross <etamin1985@yahoo.com>
2024-10-22 14:33:38 +0000

    Initialize commands package

---
+++ commands/__init__.py
@@ -0,0 +1,6 @@
+# commands/__init__.py
+
+# Initializes the commands package
+# Optionally, you could import key components for easier access:
+# from .handler import handle_command
+# from .registry import register_command
