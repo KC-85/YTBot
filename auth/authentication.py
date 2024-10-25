c26dbe2af2e1f3d48a83e602f053db89232a93fb
Kristian Cross <etamin1985@yahoo.com>
2024-10-25 20:44:07 +0000

    Add URL

--- auth/authentication.py
+++ auth/authentication.py
@@ -37,7 +37,11 @@ def initialize_youtube_client():
             else:
                 logging.info("No valid credentials available. Initiating OAuth flow...")
                 flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_FILE, SCOPES)
-                credentials = flow.run_local_server(port=0)
+
+                # Use GitPod's public URL
+                flow.redirect_uri = 'https://kc85-ytbot-odtjmcat01n.ws.codeinstitute-ide.net/'
+
+                credentials = flow.run_local_server(port=8080)
                 # Save the new credentials to the token file
                 save_credentials(credentials)
 
