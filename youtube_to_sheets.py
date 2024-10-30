8491c9934540bcd58cc3b9681c7ae36c2b466dbc
Kristian Cross <etamin1985@yahoo.com>
2024-10-30 21:54:52 +0000

    Change video id

--- youtube_to_sheets.py
+++ youtube_to_sheets.py
@@ -144,6 +144,6 @@ def main(video_id, sheet_name, interval=30):
         time.sleep(interval)
 
 if __name__ == "__main__":
-    VIDEO_ID = "Y-OMUf62FVk"  # Replace with your actual video ID
+    VIDEO_ID = "nHzpVC5foHQ"  # Replace with your actual video ID
     SHEET_NAME = "Data Spreadsheet"  # Replace with your Google Sheet's name
     main(VIDEO_ID, SHEET_NAME)
